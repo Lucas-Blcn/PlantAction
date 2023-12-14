a8904ded402a3943c395444ecfae3326bd90ee00
lucas bilancini <lucasbilancini@gmail.com>
2023-12-14 12:50:01 +0100

    models and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # Par défaut, devise permet que le passage du mail comme clé utilisable, il faut donc définir les autres
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name])
+  end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,7 @@
 class Plant < ApplicationRecord
   VALID_CATEGORIES = %w[grasse feuillue piquante ligneux herbacee vivace]
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   validates :category, presence: true, inclusion: { in: VALID_CATEGORIES }
   validates :price, presence: true, numericality: { only_integer: true }
 end
